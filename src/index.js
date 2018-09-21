d843d2323beb629c9e106e37c3616777f632f78d
Pulya10c <sergienya.oleg@yandex.ru>
2018-09-21 14:21:32 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+
+let count = 0;
+let n = Math.trunc(number/5);
+//if (n=0) {return 0;}
+while(n > 0) {
+  count = count + n;
+  n = Math.trunc(n/5);
 }
+return count;
+}
+
